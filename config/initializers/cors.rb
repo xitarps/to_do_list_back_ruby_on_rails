5dca251038207bdf97e003ed53358f3bee5b1f75
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-15 23:34:38 -0300

    [Feature]: Set production ready CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://127.0.0.1:3001'
+    origins 'http://127.0.0.1:3001', 'https://to-do-list-front-react.vercel.app'
 
     resource '*',
       headers: :any,
