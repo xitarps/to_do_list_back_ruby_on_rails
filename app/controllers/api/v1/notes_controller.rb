95973728045ac36f0bbc8579c22a4c66225c520c
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-15 23:32:14 -0300

    [Feature]: Add Notes create action

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::NotesController < Api::V1::CustomApiApplicationController
 
     render json: @notes
   end
+
+  def create
+    @note = Note.new(note_params)
+
+    return render json: @note if @note.save
+
+    render json: @note.errors
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :description, :resolving_date)
+  end
 end
